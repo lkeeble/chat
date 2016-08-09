b34f1db7041d6bd8e920a070dc060188dae15eef
Lou Keeble <lkeeble@yahoo.com>
2016-08-09 15:37:00 -0500

    Use different colors for different users.

--- chat.php
+++ chat.php
@@ -26,254 +26,276 @@
       <link rel="stylesheet" href="chat.css?v=2">
       <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ >
 <script>
-    $(chat);
+  $(chat);
 
-    function chat()	{
-    var debug = true;
-    var $messagesDiv = $('#messagesDiv');
-    var $chatTextarea = $('#chatTextarea');
-    var $handle = $('#handle');
-    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  function chat()	{
+  var debug = true;
+  var $messagesDiv = $('#messagesDiv');
+  var $chatTextarea = $('#chatTextarea');
+  var $handle = $('#handle');
+  var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+  // green, blue, black, red, orange, purple, fuchsia, brown, grey, tan
+  var handleColors = ["#00ff00", "#0000ff", "#000000", "#ff0000", "#e67e22", "#8e44ad", "#ff0080", "#8B4513", "#808080", "#D2B48C"];
 
-    function log(msg) {
-      if (debug) {
-        console.log(msg);
-      }
+  function log(msg) {
+    if (debug) {
+      console.log(msg);
     }
+  }
     
-    function initChat() {
+  function linkify(text) {
+      var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+      return text.replace(urlRegex, function(url) {
+          return '<a href="' + url + '" target="_blank">' + url + '</a>';
+      });
+  }
+
+  function getHashCode(str) {
+    var hash = 0, i, chr, len;
+    if (str.length === 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
+  
+  function getColorIndex(str) {
+    return getHashCode(str) % 10;
+  }
+  
+  function getHexColor(str) {
+    return handleColors[getColorIndex(str)];
+  }
+    
+  function initChat() {
+    
+    getAllMessages();
+    var nowISO = (new Date()).toISOString(); 
+    otherClientMessageLatestDate = nowISO;
+    var reloadOtherClientInterval = setInterval(getMessagesFromOtherClients, 1000);
+    $chatTextarea.focus();
+  }
+  
+  $chatTextarea.keypress(function(e) {
+    if (e.which == 13) {
+      var text = $chatTextarea.val();
+      var handle = $handle.val();
+      var message = Message.createChatMessage(handle, text);
       
-      getAllMessages();
-      var nowISO = (new Date()).toISOString(); 
-      otherClientMessageLatestDate = nowISO;
-      var reloadOtherClientInterval = setInterval(getMessagesFromOtherClients, 1000);
+      sendMessage(message);
+      appendMessage(message);
+      scrollDown();
+      clearChatWindow();
       $chatTextarea.focus();
-    }
-    
-    function linkify(text) {
-        var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-        return text.replace(urlRegex, function(url) {
-            return '<a href="' + url + '" target="_blank">' + url + '</a>';
-        });
-    }
-        
-    $chatTextarea.keypress(function(e) {
-      if (e.which == 13) {
-        var text = $chatTextarea.val();
-        var handle = $handle.val();
-        var message = Message.createChatMessage(handle, text);
-        
-        sendMessage(message);
-        appendMessage(message);
-        scrollDown();
-        clearChatWindow();
-        $chatTextarea.focus();
-        
-        // Stop newline from going into the textarea when the user hits enter.
-        e.preventDefault();
-      }
-    });
-    
-    function clearChatWindow() {
-        $chatTextarea.val('');
-    }
-    
-    $('#btnClear').click(function(e) {
-      clearAllMessagesOnServer();
-      clearAllMessagesOnClient();
-    });
-    
-    $('#btnSetBoard').click(function (evt) {
-      submitFormWithBoardName();         
-    });
       
-    $('#boardID').keypress(function(e) {
-        if (e.which == 13) {
-          submitFormWithBoardName();         
-          return false; <?php // prevent double submit ?>  
-        }
-    });
-    
-    function submitFormWithBoardName() {
-      var mainForm = $('#mainForm');
-      mainForm.attr("action", "chat.php?board=" + $('#boardID').val());
-      mainForm.submit();
+      // Stop newline from going into the textarea when the user hits enter.
+      e.preventDefault();
     }
+  });
+  
+  function clearChatWindow() {
+      $chatTextarea.val('');
+  }
+  
+  $('#btnClear').click(function(e) {
+    clearAllMessagesOnServer();
+    clearAllMessagesOnClient();
+  });
+  
+  $('#btnSetBoard').click(function (evt) {
+    submitFormWithBoardName();         
+  });
+    
+  $('#boardID').keypress(function(e) {
+      if (e.which == 13) {
+        submitFormWithBoardName();         
+        return false; <?php // prevent double submit ?>  
+      }
+  });
+  
+  function submitFormWithBoardName() {
+    var mainForm = $('#mainForm');
+    mainForm.attr("action", "chat.php?board=" + $('#boardID').val());
+    mainForm.submit();
+  }
 
-    function getAllMessages() {
-        $.ajax({
-          type: "GET",
-          url: "GetData.php",
-          dataType: "json",
-          data: {BoardID : $('#boardID').val()},
-          success : getMessagesSuccess,
-          error : getMessagesError
-        });
-    }
-    
-    function clearAllMessagesOnServer() {
-      // Clear the database records for this board.
+  function getAllMessages() {
       $.ajax({
         type: "GET",
-        url: "ClearData.php",
+        url: "GetData.php",
         dataType: "json",
         data: {BoardID : $('#boardID').val()},
-        success : clearAllMessagesOnServerSuccess,
-        error : clearMessagesError
+        success : getMessagesSuccess,
+        error : getMessagesError
       });
-    }
-    
-    function clearAllMessagesOnServerSuccess() {
-      sendClearMessage();
-    }
-      
-    function sendClearMessage() {
-        // Send message so that all other clients can be cleared as well.
-        var message = Message.createClearMessage();
-        sendMessage(message);
-    }
-    
-    function clearMessagesError() {
-              alert('ajax clearAllMessagesOnServer() call failed, status: ' + status);
-    }
-    
-    function getMessagesSuccess(data, status) {
-      var messagesStrArr = data.messages; // array of strings containing JSON for the messages
-      if (messagesStrArr.length == 0) {
-        return;
-      }
-      
-      var messagesArr = [];
-      for (var i = 0; i < messagesStrArr.length - 1; i++) {
-        var message = JSON.parse(messagesStrArr[i]);
-        messagesArr.push(message);
-      }
-      
-      // The creation date of the final message is tacked onto the data. 
-      otherClientMessageLatestDate = messagesStrArr[messagesStrArr.length - 1];
-      
-      for (var i = 0; i < messagesArr.length; i++) {
-        var message = messagesArr[i];
-        
-        var action = messagesArr[i].action;
-        if (action == "clear") {
-          clearAllMessagesOnClient();
-        }
-        else if (action == "chat") {
-          appendMessage(messagesArr[i])          
-        }        
-      }
-      
-      scrollDown();
-    }
-    
-    function fromISODateStrToLocalDateStr(isoDateStr) {
-      var d = new Date(isoDateStr);
-      return d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear() + " " + forceTwoDigits(d.getHours()) + ":" + forceTwoDigits(d.getMinutes()) + ":" + forceTwoDigits(d.getSeconds());       
-    }
-    
-    function forceTwoDigits(i)
-    {
-      if (i < 10) {
-        return "0" + i.toString();
-      }
-      
-      return i.toString();
-    }
-    
-    function testConvertDate() {
-      var convertedData = fromISODateStrToLocalDateStr('2016-08-09T16:42:50.221Z');
-      var i = 0;
-    }
-    
-    function appendMessage(message) {
-      var text = message.text;
-      text = linkify(text);
-      var handle = message.handle;
-      var messageDate = message.nowISO;
-      var messageDateLocal = fromISODateStrToLocalDateStr(messageDate);
-      var messageText = '(' + messageDateLocal + ') <span class="handle">' + message.handle + '</span>: ' + text;
-      var $messageDiv = $('<div>' + messageText + '</div><br>');
-      $messagesDiv.append($messageDiv);
-    }
-    
-    function clearAllMessagesOnClient() {
-      $messagesDiv.empty();
-    }
-      
-    function getMessagesError(jqXHR, status, error) {
-      alert('ajax getMessagesFromOtherClients() call failed, status: ' + status);
-    }
-    
-    function getMessagesFromOtherClients() {
-      $.ajax({
-      type: "GET",
-      url: "GetData.php",
-      dataType: "json",
-      data: {BoardID : $('#boardID').val(), ClientID : $('#clientID').val(), BeginDate : otherClientMessageLatestDate},
-      success : getMessagesSuccess,
-      error : getMessagesError
-      });
-    }
-      
-    function submitFormWithBoardName() {
-      var mainForm = $('#mainForm');
-      mainForm.attr("action", "chat.php?board=" + $('#boardID').val());
-      mainForm.submit();
-    }
-    
-    function sendMessage(message) {
-        var boardID = $('#boardID').val();
-        var clientID = $('#clientID').val(); // replace with <?php session_id() ?> when done debugging.
-
-        sendMessageData(boardID, clientID, message);
-    }
-    
-    function sendMessageData(boardID, clientID, message) {
-      $.ajax({
-        type: "POST",
-        url: "StoreData.php",
-        data: {BoardID : boardID, ClientID : clientID, MessageData : JSON.stringify(message)},
-        success : sendMessageSuccess,
-        error : sendMessageError
-      });
-    }
-    
-    function sendMessageSuccess() {
-      console.log("sendMessageData success.");
-    }
-    
-    function sendMessageError(xhr, ajaxOptions, thrownError) {
-      alert("sendMessageData error: " + thrownError);
-    }
-    
-    function scrollDown() { 
-      $messagesDiv.scrollTop($messagesDiv[0].scrollHeight);
-    }
-    
-    function Message() {
-    }
-
-    Message.createChatMessage = function(handle, text) {
-      var s = new Message();
-      s.action = "chat";
-      s.handle = handle;
-      s.text = text;
-      var nowISO = (new Date()).toISOString();
-      s.nowISO = nowISO;
-      
-      return s; 
-    }
-    
-    Message.createClearMessage = function() {
-      var s = new Message();
-      s.action = "clear";
-      
-      return s;
-    }
-    
-    initChat();
   }
+  
+  function clearAllMessagesOnServer() {
+    // Clear the database records for this board.
+    $.ajax({
+      type: "GET",
+      url: "ClearData.php",
+      dataType: "json",
+      data: {BoardID : $('#boardID').val()},
+      success : clearAllMessagesOnServerSuccess,
+      error : clearMessagesError
+    });
+  }
+  
+  function clearAllMessagesOnServerSuccess() {
+    sendClearMessage();
+  }
+    
+  function sendClearMessage() {
+      // Send message so that all other clients can be cleared as well.
+      var message = Message.createClearMessage();
+      sendMessage(message);
+  }
+  
+  function clearMessagesError() {
+            alert('ajax clearAllMessagesOnServer() call failed, status: ' + status);
+  }
+  
+  function getMessagesSuccess(data, status) {
+    var messagesStrArr = data.messages; // array of strings containing JSON for the messages
+    if (messagesStrArr.length == 0) {
+      return;
+    }
+    
+    var messagesArr = [];
+    for (var i = 0; i < messagesStrArr.length - 1; i++) {
+      var message = JSON.parse(messagesStrArr[i]);
+      messagesArr.push(message);
+    }
+    
+    // The creation date of the final message is tacked onto the data. 
+    otherClientMessageLatestDate = messagesStrArr[messagesStrArr.length - 1];
+    
+    for (var i = 0; i < messagesArr.length; i++) {
+      var message = messagesArr[i];
+      
+      var action = messagesArr[i].action;
+      if (action == "clear") {
+        clearAllMessagesOnClient();
+      }
+      else if (action == "chat") {
+        appendMessage(messagesArr[i])          
+      }        
+    }
+    
+    scrollDown();
+  }
+  
+  function fromISODateStrToLocalDateStr(isoDateStr) {
+    var d = new Date(isoDateStr);
+    return d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear() + " " + forceTwoDigits(d.getHours()) + ":" + forceTwoDigits(d.getMinutes()) + ":" + forceTwoDigits(d.getSeconds());       
+  }
+  
+  function forceTwoDigits(i)
+  {
+    if (i < 10) {
+      return "0" + i.toString();
+    }
+    
+    return i.toString();
+  }
+  
+  function testConvertDate() {
+    var convertedData = fromISODateStrToLocalDateStr('2016-08-09T16:42:50.221Z');
+    var i = 0;
+  }
+  
+  function appendMessage(message) {
+    var text = message.text;
+    text = linkify(text);
+    var handle = message.handle;
+    var messageDate = message.nowISO;
+    var messageDateLocal = fromISODateStrToLocalDateStr(messageDate);
+    var messageText = '(' + messageDateLocal + ') <span class="handle">' + message.handle + '</span>: ' + text;
+    var $messageDiv = $('<div>' + messageText + '</div><br>');
+    $messageDiv.find(".handle").css('color', getHexColor(handle));
+    $messagesDiv.append($messageDiv);
+  }
+  
+  function clearAllMessagesOnClient() {
+    $messagesDiv.empty();
+  }
+    
+  function getMessagesError(jqXHR, status, error) {
+    alert('ajax getMessagesFromOtherClients() call failed, status: ' + status);
+  }
+  
+  function getMessagesFromOtherClients() {
+    $.ajax({
+    type: "GET",
+    url: "GetData.php",
+    dataType: "json",
+    data: {BoardID : $('#boardID').val(), ClientID : $('#clientID').val(), BeginDate : otherClientMessageLatestDate},
+    success : getMessagesSuccess,
+    error : getMessagesError
+    });
+  }
+    
+  function submitFormWithBoardName() {
+    var mainForm = $('#mainForm');
+    mainForm.attr("action", "chat.php?board=" + $('#boardID').val());
+    mainForm.submit();
+  }
+  
+  function sendMessage(message) {
+      var boardID = $('#boardID').val();
+      var clientID = $('#clientID').val(); // replace with <?php session_id() ?> when done debugging.
+
+      sendMessageData(boardID, clientID, message);
+  }
+  
+  function sendMessageData(boardID, clientID, message) {
+    $.ajax({
+      type: "POST",
+      url: "StoreData.php",
+      data: {BoardID : boardID, ClientID : clientID, MessageData : JSON.stringify(message)},
+      success : sendMessageSuccess,
+      error : sendMessageError
+    });
+  }
+  
+  function sendMessageSuccess() {
+    console.log("sendMessageData success.");
+  }
+  
+  function sendMessageError(xhr, ajaxOptions, thrownError) {
+    alert("sendMessageData error: " + thrownError);
+  }
+  
+  function scrollDown() { 
+    $messagesDiv.scrollTop($messagesDiv[0].scrollHeight);
+  }
+  
+  function Message() {
+  }
+
+  Message.createChatMessage = function(handle, text) {
+    var s = new Message();
+    s.action = "chat";
+    s.handle = handle;
+    s.text = text;
+    var nowISO = (new Date()).toISOString();
+    s.nowISO = nowISO;
+    
+    return s; 
+  }
+  
+  Message.createClearMessage = function() {
+    var s = new Message();
+    s.action = "clear";
+    
+    return s;
+  }
+  
+  initChat();
+}
     </script>
   </head>
 
