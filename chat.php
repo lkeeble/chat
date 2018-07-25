268e124eca699dede3da03e0525ffe6a52b1d7af
Lou Keeble <lkeeble@yahoo.com>
2018-07-24 21:13:40 -0500

    Use https to access the google jquery library.

--- chat.php
+++ chat.php
@@ -21,7 +21,7 @@
   <head>
       <meta http-equiv="Content-type" content="text/html; charset=utf-8">
       <title>Chat</title>
-      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
       <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
       <link rel="stylesheet" href="chat.css?v=<?php echo time();?>">
       <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ >
